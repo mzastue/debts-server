7ec0f72da24bb8a1417b0be8c86c3032591b3cf3
mateusz.littwin <mateusz.littwin@gforces.pl>
2018-09-28 18:49:55 +0200

    Fetch unique lenders from debtor

--- src/models/user.js
+++ src/models/user.js
@@ -7,14 +7,24 @@ export const typeDef = gql`
         name: String
         surname: String
         allDebts: [Debt]
+        allLenders: [User]
     }
 `;
 
 export const resolver = {
   User: {
-    allDebts: (root, args) => {
+    allDebts: (root) => {
       return User.findById(root.id)
         .then(user => user.debts);
     },
+
+    allLenders: async (root) => {
+      const lenderIds = await User.findById(root.id)
+        .then(user => {
+          return JSON.parse(JSON.stringify(user.debts)).map(d => d.lenderId);
+        });
+      const uniqueLendersIds = Array.from(new Set(lenderIds));
+      return uniqueLendersIds.map(lenderId => User.findById(lenderId));
+    }
   }
 };
